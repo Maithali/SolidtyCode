a8c25e0ce9c0f88a23d67b37dff4c4575da7056f
Maithali <121287753+Maithali@users.noreply.github.com>
2022-12-24 10:21:05 +0530

    Create SwappingwithThirdVariable.sol

---
+++ SwappingwithThirdVariable.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
+
+contract Find{
+
+function swap(uint num1, uint num2)public pure returns(uint, uint){
+uint temp;
+num1=temp;
+num1=num2;
+num2=temp;
+return (num1,num2);
+}
+
+}
