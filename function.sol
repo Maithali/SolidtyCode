df35f55c04bf71d77211e16aef056a7c2f5e0387
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-12 13:36:35 +0530

    Create function.sol

---
+++ function.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.1;
+
+contract Demo{
+
+uint public n2=15;// public creates automatic getter  function.
+    function setter() public pure returns(uint) {
+      uint  n=5;
+      return n;
+    }
+    
+}
