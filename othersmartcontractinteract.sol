bbaef1b8646ce9c6cdc84c86b1c1ebbef671f6ff
Maithali <121287753+Maithali@users.noreply.github.com>
2023-03-18 15:46:13 +0530

    smart contract for keeping my screct private
    using inheritance here we are interacting with other smart contract but the secretVault is  keeping is hidden from other accounts

---
+++ othersmartcontractinteract.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
+
+contract Ownable{
+    address owner;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor(){
+        owner= msg.sender;
+    }
+
+}
+
+contract SecretVault{
+    string secret;
+
+     constructor(string memory _secret){
+        secret =_secret;
+    }
+    function getSecret() public view returns(string memory){
+        return secret;
+    }
+}
+
+contract MyContract is Ownable{
+    address secretVault;
+
+   constructor(string memory _secret){
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+    function getSecret() public view onlyOwner returns(string memory){
+        return SecretVault(secretVault).getSecret();
+    }
+}
