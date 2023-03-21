8ca966a49000d38ab9492e437a7a6ce00d2bc4ec
Maithali <121287753+Maithali@users.noreply.github.com>
2023-03-21 10:25:14 +0530

    solidity value types

---
+++ Values.sol
@@ -0,0 +1,26 @@
+/SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
+
+contract Variables {
+    //boolean only true or false
+    bool public myBool=true;
+
+    //interger negavtive value is supported
+    int public myInt = -3;
+
+    //uint no negative value
+    uint public myUint= 3;
+
+    //string any value
+    string public myString ="Maithali";
+
+    //Bytes are like string
+    bytes4 public myBytes ="Mait";
+
+    //address 
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    
+
+
+}
