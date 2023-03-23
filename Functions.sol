3f8c79b980da57b6eee9e9a56e20b46b7291d1a4
Maithali <121287753+Maithali@users.noreply.github.com>
2023-03-23 05:32:45 +0530

    basic function

---
+++ Functions.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
+
+contract functions {
+   uint public val = 4;
+   function add() public  pure returns(uint){
+       return 2+3;
+
+   }
+
+}
