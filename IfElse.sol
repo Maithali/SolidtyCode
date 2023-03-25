3d2d7aa9c54e726c489aab91eb9da95ece54732b
Maithali <121287753+Maithali@users.noreply.github.com>
2023-03-25 08:14:39 +0530

    If Else

---
+++ IfElse.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10;
+
+contract Condition{
+
+    uint public a = 10;
+    uint public b = 15;
+
+    function check() public view returns(uint){
+        if(a==15){
+            return 0;
+        }else {
+
+        return 1;
+        }
+    }
+
+    
+}
