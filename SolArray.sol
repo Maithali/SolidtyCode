48f1842e81685b3b4a66f714aa22f813f9bdeb5b
Maithali <121287753+Maithali@users.noreply.github.com>
2023-03-27 07:51:29 +0530

    Update SolArray.sol

--- SolArray.sol
+++ SolArray.sol
@@ -8,6 +8,21 @@ contract Array {
 //update at fixed index
        myArray[2]=22;
        return myArray[1];
-
    }
+
+       function getLenAry() public view returns(uint){
+           //get the leght of array
+           return myArray.length;//5
+       }
+
+        function insertArry() public{
+                myArray.push(45);
+                 myArray.push(16);
+                myArray.push(25);
+}
+
+function deleteArry() public{
+    myArray.pop();
+}
+   
 }
