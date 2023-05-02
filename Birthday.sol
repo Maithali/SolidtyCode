7f867968fb262afed04c2ec8a57e2c53d518f6f2
Maithali <121287753+Maithali@users.noreply.github.com>
2023-05-02 20:25:14 +0530

    Update Birthday.sol

--- Birthday.sol
+++ Birthday.sol
@@ -13,11 +13,92 @@ contract SelectBirthdayMonth{
         select = Month.feb;
     }
 
+    function setMar()public{
+        select = Month.mar;
+    }
+
+    function setApril() public{
+        select = Month.april;
+    }
+
+    function setMay() public{
+        select = Month.may;
+    }
+
+    function setJune() public{
+        select = Month.june;
+    }
+
+    function setJuly() public{
+        select = Month.july;
+    }
+
+    function setAug() public{
+        select = Month.aug;
+    }
+
+    function setSept() public{
+        select = Month.sep;
+    }
+
+    function setOct() public{
+        select = Month.oct;
+    }
+
+    function setNov() public{
+        select = Month.nov;
+    }
+
+    function setDec() public{
+        select = Month.dec;
+    }
+
+
     function getJan() public view returns(Month){
        return select;
     }
 
     function getFeb() public view returns(Month){
+        return select;
+    }
+
+    function getMar() public view returns(Month){
+        return select;
+    }
+
+    function getApril() public view returns(Month){
+        return select;
+    }
+
+    function getMay() public view returns(Month){
+        return select;
+    }
+
+    function getJune() public view returns(Month){
+        return select;
+    }
+
+    function getJuly() public view returns(Month){
+        return select;
+    }
+
+    function getAug() public view returns(Month){
+        return select;
+    }
+
+    function getSept() public view returns(Month){
+        return select;
+    }
+
+    function getOct() public view returns(Month){
+        return select;
+    }
+
+    function getNov() public view returns(Month){
+        return select;
+    }
+
+    function getDec()public view returns(Month){
         return select;
     }
 }
