4883a6df18c083c85c7e2c22ecb48ce4bf611cf5
Maithali <121287753+Maithali@users.noreply.github.com>
2023-05-05 08:10:23 +0530

    Update colour.sol

--- colour.sol
+++ colour.sol
@@ -11,7 +11,50 @@ contract Color{
         select = Vibgyor.voilet;
     }
 
+    function setIndigo() public{
+        select = Vibgyor.indigo;
+    }
+
+    function setBlue() public{
+        select = Vibgyor.blue;
+    }
+
+    function setGreen() public{
+        select = Vibgyor.green;
+    }
+
+    function setYellow() public{
+        select = Vibgyor.yellow;
+    }
+    function setOrange() public{
+        select = Vibgyor.orange;
+    }
+    function setRed() public{
+        select = Vibgyor.red;
+    }
+    function getYellow() public view returns(Vibgyor){
+        return select;
+    }
+    function getOrange()public view returns(Vibgyor){
+        return select;
+    }
+    function getRed() public view returns(Vibgyor){
+        return select;
+    }
+
+    function getGreen() public view returns(Vibgyor){
+       return select;
+    }
+
+
+    function getIndigo() public view returns(Vibgyor){
+        return select;
+    }
     function getVoilet() public view returns(Vibgyor){
+        return select;
+    }
+
+    function getBlue() public view returns(Vibgyor){
         return select;
     }
 }
