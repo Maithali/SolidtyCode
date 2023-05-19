6284a03cff61e99586734d16b5ad9740ac70be03
Maithali <121287753+Maithali@users.noreply.github.com>
2023-05-19 05:55:44 +0530

    Trying Boolean

---
+++ BooleanPract.sol
@@ -0,0 +1,12 @@
+//SPDX-License_identifer:MIT
+
+pragma solidity ^0.8.1;
+
+
+contract BooleanEample{
+    bool public myBool;
+
+    function setMyBool(bool _myBool)public{
+        myBool = _myBool;
+    }
+} 
